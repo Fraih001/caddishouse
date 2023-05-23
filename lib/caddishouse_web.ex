b5049fd973d32f279599d177c791c07d70a658d0
Fraih001 <90857197+Fraih001@users.noreply.github.com>
2023-05-23 19:35:34 -0400

    new

---
+++ lib/caddishouse_web.ex
@@ -0,0 +1,114 @@
+defmodule CaddishouseWeb do
+  @moduledoc """
+  The entrypoint for defining your web interface, such
+  as controllers, views, channels and so on.
+
+  This can be used in your application as:
+
+      use CaddishouseWeb, :controller
+      use CaddishouseWeb, :view
+
+  The definitions below will be executed for every view,
+  controller, etc, so keep them short and clean, focused
+  on imports, uses and aliases.
+
+  Do NOT define functions inside the quoted expressions
+  below. Instead, define any helper function in modules
+  and import those modules here.
+  """
+  def static_paths, do: ~w(assets fonts images icons robots.txt)
+
+  def controller do
+    quote do
+      use Phoenix.Controller, namespace: CaddishouseWeb
+
+      import Plug.Conn
+      import CaddishouseWeb.Gettext
+      alias CaddishouseWeb.Router.Helpers, as: Routes
+      unquote(verified_routes())
+    end
+  end
+
+  def view do
+    quote do
+      use Phoenix.View,
+        root: "lib/caddishouse_web/templates",
+        namespace: CaddishouseWeb
+
+      # Import convenience functions from controllers
+      import Phoenix.Controller,
+        only: [get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
+
+      # Include shared imports and aliases for views
+      unquote(view_helpers())
+      unquote(verified_routes())
+    end
+  end
+
+  def live_view do
+    quote do
+      use Phoenix.LiveView,
+        layout: {CaddishouseWeb.LayoutView, "live.html"}
+
+      unquote(view_helpers())
+    end
+  end
+
+  def live_component do
+    quote do
+      use Phoenix.LiveComponent
+
+      unquote(view_helpers())
+    end
+  end
+
+  def router do
+    quote do
+      use Phoenix.Router
+
+      import Plug.Conn
+      import Phoenix.Controller
+      import Phoenix.LiveView.Router
+    end
+  end
+
+  def channel do
+    quote do
+      use Phoenix.Channel
+      import CaddishouseWeb.Gettext
+    end
+  end
+
+  defp view_helpers do
+    quote do
+      # Import basic rendering functionality (render, render_layout, etc)
+      use Phoenix.HTML
+      import Phoenix.View
+      import Phoenix.Component
+      use Phoenix.HTML
+      import CaddishouseWeb.Live.Helpers
+
+      import CaddishouseWeb.ErrorHelpers
+      import CaddishouseWeb.Gettext
+      alias CaddishouseWeb.Router.Helpers, as: Routes
+      alias Phoenix.LiveView.JS
+      alias CaddishouseWeb.Live.Components
+    end
+  end
+
+  def verified_routes do
+    quote do
+      use Phoenix.VerifiedRoutes,
+        endpoint: CaddishouseWeb.Endpoint,
+        router: CaddishouseWeb.Router,
+        statics: CaddishouseWeb.static_paths()
+    end
+  end
+
+  @doc """
+  When used, dispatch to the appropriate controller/view/etc.
+  """
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
+end
